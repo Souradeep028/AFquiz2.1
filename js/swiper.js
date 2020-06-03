a7515a4f0d464a50dc58d11473bf48a90c00f63f
Souradeep <souradeepkrbasu@gmail.com>
2020-06-03 09:58:39 +0530

    Initial Upload

---
+++ js/swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper( '.swiper-container.two', {
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    effect: 'coverflow',
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflow: {
+        rotate: 0,
+        stretch: 100,
+        depth: 60,
+        modifier: 1.5,
+        slideShadows : false,
+    }
+} );
