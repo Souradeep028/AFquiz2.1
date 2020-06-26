5ec8601c9a62fa9d6909321871e73bc6020e8f93
Suraj Mandal <surajmandalcell@gmail.com>
2020-06-26 14:32:28 +0530

    fiexed cors error and added images

--- js/swiper.js
+++ js/swiper.js
@@ -1,19 +1,15 @@
-var swiper = new Swiper( '.swiper-container.two', {
-    // navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    // },
-    pagination: '.swiper-pagination',
-    paginationClickable: true,
-    effect: 'coverflow',
-    loop: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflow: {
-        rotate: 0,
-        stretch: 100,
-        depth: 15,
-        modifier: 1.5,
-        slideShadows : false,
-    }
-} );
+var swiper = new Swiper(".swiper-container.two", {
+  pagination: ".swiper-pagination",
+  paginationClickable: true,
+  effect: "coverflow",
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflow: {
+    rotate: 0,
+    stretch: 100,
+    depth: 15,
+    modifier: 1.5,
+    slideShadows: false,
+  },
+});
