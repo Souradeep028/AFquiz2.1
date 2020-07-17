9095c8d7ed1b8352430e95377b0e4aee0e5e8aa9
Suraj Mandal <surajmandalcell@gmail.com>
2020-07-17 16:10:59 +0530

    remove npm

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,10 @@
-var swiper = new Swiper(".swiper-container.two", {
-  pagination: ".swiper-pagination",
+var swiper = new Swiper('.swiper-container.two', {
+  pagination: '.swiper-pagination',
   paginationClickable: true,
-  effect: "coverflow",
+  effect: 'coverflow',
   loop: true,
   centeredSlides: true,
-  slidesPerView: "auto",
+  slidesPerView: 'auto',
   // coverflow: {
   //   rotate: 0,
   //   stretch: 100,
