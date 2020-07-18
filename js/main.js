c032f1bde5a4c1bb3f73447fe19f829c97d2ebf9
Suraj Mandal <surajmandalcell@gmail.com>
2020-07-18 10:00:38 +0530

    make seperate pages for each art personality

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 // $('#q1container').hide();
-$('#q2container').hide();
-$('#q3container').hide();
-$('#q4container').hide();
-$('#q5container').hide();
-$('#q6container').hide();
-$('#q7container').hide();
-var pl1 = $('#pl-1');
-var pl2 = $('#pl-2');
-var pl3 = $('#pl-3');
+$("#q2container").hide();
+$("#q3container").hide();
+$("#q4container").hide();
+$("#q5container").hide();
+$("#q6container").hide();
+$("#q7container").hide();
+var pl1 = $("#pl-1");
+var pl2 = $("#pl-2");
+var pl3 = $("#pl-3");
 pl1.hide();
 pl2.hide();
 pl3.hide();
-$('#emailcontainer').hide();
-$('#endingcontainer').hide();
+$("#emailcontainer").hide();
+$("#endingcontainer").hide();
 
 let T = 0;
 let C = 0;
 
-$('#sitetitle').text(d.site.title);
+$("#sitetitle").text(d.site.title);
 
 // $('#headernav').append(`<img src="./img/af-logo.png" id="aflogonav">`);
 
@@ -33,8 +33,8 @@ $('#sitetitle').text(d.site.title);
 //         </div>`;
 // }, ''));
 
-$('#q1h').text(d.q1.heading);
-$('#q1row').html(
+$("#q1h").text(d.q1.heading);
+$("#q1row").html(
   d.q1.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -45,11 +45,11 @@ $('#q1row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q2h').text(d.q2.heading);
-$('#q2row').html(
+$("#q2h").text(d.q2.heading);
+$("#q2row").html(
   d.q2.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -60,11 +60,11 @@ $('#q2row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q3h').text(d.q3.heading);
-$('#q3row').html(
+$("#q3h").text(d.q3.heading);
+$("#q3row").html(
   d.q3.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -75,11 +75,11 @@ $('#q3row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q4h').text(d.q4.heading);
-$('#q4row').html(
+$("#q4h").text(d.q4.heading);
+$("#q4row").html(
   d.q4.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -90,11 +90,11 @@ $('#q4row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q5h').text(d.q5.heading);
-$('#q5row').html(
+$("#q5h").text(d.q5.heading);
+$("#q5row").html(
   d.q5.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -105,11 +105,11 @@ $('#q5row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q6h').text(d.q6.heading);
-$('#q6row').html(
+$("#q6h").text(d.q6.heading);
+$("#q6row").html(
   d.q6.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -120,11 +120,11 @@ $('#q6row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#q7h').text(d.q7.heading);
-$('#q7row').html(
+$("#q7h").text(d.q7.heading);
+$("#q7row").html(
   d.q7.answerlist.reduce(function (acc, c) {
     return (
       acc +
@@ -135,19 +135,19 @@ $('#q7row').html(
             <br>
         </div>`
     );
-  }, '')
+  }, "")
 );
 
-$('#emailh').text(d.emailform.heading);
+$("#emailh").text(d.emailform.heading);
 
 $(document).ready(function () {
-  var q = ['', '', '', ''];
+  var q = ["", "", "", ""];
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  var type = '';
-  var emailcollected = window.location.search.includes('collected');
-  $('.q1').click(function () {
-    q[0] = $(this).attr('id');
-    if (q[0] == 'q1a') {
+  var type = "";
+  var emailcollected = window.location.search.includes("collected");
+  $(".q1").click(function () {
+    q[0] = $(this).attr("id");
+    if (q[0] == "q1a") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -156,15 +156,15 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q1container').hide(400);
-    $('#q2container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q1container").hide(400);
+    $("#q2container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q2').click(function () {
-    q[1] = $(this).attr('id');
-    if (q[1] == 'q2b' || q[1] == 'q2c') {
+  $(".q2").click(function () {
+    q[1] = $(this).attr("id");
+    if (q[1] == "q2b" || q[1] == "q2c") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -173,15 +173,15 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q2container').hide(400);
-    $('#q3container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q2container").hide(400);
+    $("#q3container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q3').click(function () {
-    q[2] = $(this).attr('id');
-    if (q[2] == 'q3a') {
+  $(".q3").click(function () {
+    q[2] = $(this).attr("id");
+    if (q[2] == "q3a") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -190,15 +190,15 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q3container').hide(400);
-    $('#q4container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q3container").hide(400);
+    $("#q4container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q4').click(function () {
-    q[3] = $(this).attr('id');
-    if (q[3] == 'q4b' || q[3] == 'q4c') {
+  $(".q4").click(function () {
+    q[3] = $(this).attr("id");
+    if (q[3] == "q4b" || q[3] == "q4c") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -207,15 +207,15 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q4container').hide(400);
-    $('#q5container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q4container").hide(400);
+    $("#q5container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q5').click(function () {
-    q[4] = $(this).attr('id');
-    if (q[4] == 'q5a') {
+  $(".q5").click(function () {
+    q[4] = $(this).attr("id");
+    if (q[4] == "q5a") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -224,15 +224,15 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q5container').hide(400);
-    $('#q6container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q5container").hide(400);
+    $("#q6container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q6').click(function () {
-    q[5] = $(this).attr('id');
-    if (q[5] == 'q6a' || q[5] == 'q6c') {
+  $(".q6").click(function () {
+    q[5] = $(this).attr("id");
+    if (q[5] == "q6a" || q[5] == "q6c") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
@@ -241,115 +241,118 @@ $(document).ready(function () {
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
-    $('#q6container').hide(400);
-    $('#q7container').show(700);
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#q6container").hide(400);
+    $("#q7container").show(700);
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
   });
-  $('.q7').click(function () {
-    q[6] = $(this).attr('id');
-    if (q[6] == 'q7a') {
+  $(".q7").click(function () {
+    q[6] = $(this).attr("id");
+    if (q[6] == "q7a") {
       T++;
       // console.log("T value is = ", T);
       // console.log("C value is = ", C);
     }
     if (T == 6) {
-      console.log('T value is = ', T + 1);
-      console.log('C value is = ', C);
+      console.log("T value is = ", T + 1);
+      console.log("C value is = ", C);
     } else if (C == 6) {
-      console.log('T value is = ', T);
-      console.log('C value is = ', C + 1);
+      console.log("T value is = ", T);
+      console.log("C value is = ", C + 1);
     } else {
-      console.log('T value is = ', T);
-      console.log('C value is = ', C);
+      console.log("T value is = ", T);
+      console.log("C value is = ", C);
     }
-    $('#q7container').hide(400);
+    $("#q7container").hide(400);
     if (emailcollected) {
-      $('#endingcontainer').show(700);
+      $("#endingcontainer").show(700);
     } else {
-      $('#emailcontainer').show(700);
+      $("#emailcontainer").show(700);
     }
-    $('#headernav').append(
-      `<img src="${$(this).find('img').attr('src')}" id="aflogonav">`
+    $("#headernav").append(
+      `<img src="${$(this).find("img").attr("src")}" id="aflogonav">`
     );
     var o1 = d.outcomes[0];
     var o2 = d.outcomes[1];
     var o3 = d.outcomes[2];
     var outcome;
     if (T >= 5) {
-      $('#endingh').text(o1.heading);
-      $('#endingp').text(o1.paragraph);
-      $('#guideimg').prop('src', './img' + '/' + o1.img);
+      $("#endingh").text(o1.heading);
+      $("#endingp").text(o1.paragraph);
+      $("#guideimg").prop("src", "./img" + "/" + o1.img);
       outcome = o1.id;
-      $('#pl-1').show(700);
-      console.log('You are a Traditionalist.');
+      $("#pl-1").show(700);
+      console.log(emailcollected);
+      console.log("You are a Traditionalist.");
     } else if (T >= 0 && T <= 2) {
-      $('#endingh').text(o2.heading);
-      $('#endingp').text(o2.paragraph);
-      $('#guideimg').prop('src', './img' + '/' + o2.img);
+      $("#endingh").text(o2.heading);
+      $("#endingp").text(o2.paragraph);
+      $("#guideimg").prop("src", "./img" + "/" + o2.img);
       outcome = o2.id;
-      $('#pl-2').show(700);
-      console.log('You are a New Age.');
+      $("#pl-2").show(700);
+      console.log(emailcollected);
+      console.log("You are a New Age.");
     } else {
-      $('#endingh').text(o3.heading);
-      $('#endingp').text(o3.paragraph);
-      $('#guideimg').prop('src', './img' + '/' + o3.img);
+      $("#endingh").text(o3.heading);
+      $("#endingp").text(o3.paragraph);
+      $("#guideimg").prop("src", "./img" + "/" + o3.img);
       outcome = o3.id;
-      $('#pl-3').show(700);
-      console.log('You are a Mixed Bag.');
+      $("#pl-3").show(700);
+      console.log(emailcollected);
+      console.log("You are a Mixed Bag.");
     }
     // $('#cinput').val(city);
-    $('#q1input').val(q[0]);
-    $('#q2input').val(q[1]);
-    $('#q3input').val(q[2]);
-    $('#q4input').val(q[3]);
-    $('#q5input').val(q[4]);
-    $('#q6input').val(q[5]);
-    $('#q7input').val(q[6]);
-    $('#outcomeinput').val(outcome);
+    $("#q1input").val(q[0]);
+    $("#q2input").val(q[1]);
+    $("#q3input").val(q[2]);
+    $("#q4input").val(q[3]);
+    $("#q5input").val(q[4]);
+    $("#q6input").val(q[5]);
+    $("#q7input").val(q[6]);
+    $("#outcomeinput").val(outcome);
   });
-  $('#submitemail').prop('disabled', true);
-  $('#inputemail').keyup(function () {
+  $("#submitemail").prop("disabled", true);
+  $("#inputemail").keyup(function () {
     var email = String($(this).val());
     if (re.test(email.toLowerCase())) {
-      $('#submitemail').prop('disabled', false);
+      $("#submitemail").prop("disabled", false);
       // $('#cinput').prop('disabled', false);
-      $('#q1input').prop('disabled', false);
-      $('#q2input').prop('disabled', false);
-      $('#q3input').prop('disabled', false);
-      $('#q4input').prop('disabled', false);
-      $('#q5input').prop('disabled', false);
-      $('#q6input').prop('disabled', false);
-      $('#q7input').prop('disabled', false);
-      $('#outcomeinput').prop('disabled', false);
+      $("#q1input").prop("disabled", false);
+      $("#q2input").prop("disabled", false);
+      $("#q3input").prop("disabled", false);
+      $("#q4input").prop("disabled", false);
+      $("#q5input").prop("disabled", false);
+      $("#q6input").prop("disabled", false);
+      $("#q7input").prop("disabled", false);
+      $("#outcomeinput").prop("disabled", false);
     }
   });
 
   const scriptURL =
-    'https://script.google.com/macros/s/AKfycbz45bm4_cacReXs_pSKmvMIO46ZV9IOthU7Mcf-ZBlxBT3E_FPB/execgu';
-  const form = document.forms['quizinfo'];
+    "https://script.google.com/macros/s/AKfycbz45bm4_cacReXs_pSKmvMIO46ZV9IOthU7Mcf-ZBlxBT3E_FPB/execgu";
+  const form = document.forms["quizinfo"];
 
-  $('#emailform').submit(function (e) {
+  $("#emailform").submit(function (e) {
     e.preventDefault();
     // var $form = $(this);
-    $('#submitemail').attr('disabled', true);
+    $("#submitemail").attr("disabled", true);
     fetch(scriptURL, {
-      method: 'POST',
+      method: "POST",
       body: new FormData(form),
     })
       .then((response) => {
         //   $("#emailcontainer").hide(40);
         //   $("#endingcontainer").show(700);
-        console.log('Success!', response);
+        console.log("Success!", response);
       })
-      .catch((error) => console.error('Error!', error.message));
+      .catch((error) => console.error("Error!", error.message));
 
-    $('#headernav').hide();
-    $('#emailcontainer').hide(40);
-    $('#headercontainer').css({ display: 'flex', 'justify-content': 'center' });
-    $('#aflogo').css({ height: '48px' });
-    $('#endingcontainer').show(700);
+    $("#headernav").hide();
+    $("#emailcontainer").hide(40);
+    $("#headercontainer").css({ display: "flex", "justify-content": "center" });
+    $("#aflogo").css({ height: "48px" });
+    $("#endingcontainer").show(700);
 
     // $.post($form.attr("action"), $form.serialize()).then(function() {
     //     emailcollected = true;
@@ -367,13 +370,13 @@ $(document).ready(function () {
   //     });
   // });
 
-  $('#restart').click(function () {
-    q = ['', '', ''];
+  $("#restart").click(function () {
+    q = ["", "", ""];
     T = 0;
     C = 0;
-    $('#endingcontainer').hide();
-    $('#q1container').show();
-    $('#headernav').empty();
-    $('#headernav').append('<img src="./img/af-logo.png" id="aflogonav">');
+    $("#endingcontainer").hide();
+    $("#q1container").show();
+    $("#headernav").empty();
+    $("#headernav").append('<img src="./img/af-logo.png" id="aflogonav">');
   });
 });
--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,14 @@
-var swiper = new Swiper('.swiper-container.two', {
-  pagination: '.swiper-pagination',
+var swiper = new Swiper(".swiper-container.two", {
+  pagination: ".swiper-pagination",
   paginationClickable: true,
-  effect: 'coverflow',
+  effect: "coverflow",
   loop: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   // coverflow: {
   //   rotate: 0,
-  //   stretch: 100,
   //   depth: 15,
+  //   stretch: 100,
   //   modifier: 1.5,
   //   slideShadows: false,
   // },
